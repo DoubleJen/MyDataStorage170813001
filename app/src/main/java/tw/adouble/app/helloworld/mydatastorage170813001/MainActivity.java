71448736ea60794eed0704e3622a222a72742def
Chang <qoo2003654@gmail.com>
2017-08-13 13:52:06 +0800

    170813 1351

--- app/src/main/java/tw/adouble/app/helloworld/mydatastorage170813001/MainActivity.java
+++ app/src/main/java/tw/adouble/app/helloworld/mydatastorage170813001/MainActivity.java
@@ -1,7 +1,12 @@
 package tw.adouble.app.helloworld.mydatastorage170813001;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }else{
+            init();
+        }
+    }
+
+    private void  init(){
         sp = getSharedPreferences("gamedata", MODE_PRIVATE); //以xml型式儲存
         editor = sp.edit();
 
@@ -38,6 +55,20 @@ public class MainActivity extends AppCompatActivity {
         approot = new File(sdroot, "Android/data/" + getPackageName() + "/");
         if (!approot.exists()){
             approot.mkdirs(); //mkdirs=>父不在自動弄一個
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 123){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                init();
+            }else {
+                finish();
+            }
         }
 
     }
@@ -82,6 +113,31 @@ public class MainActivity extends AppCompatActivity {
             }
             Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show();
         }catch (Exception e){
+            Log.i("brad", e.toString());
+        }
+    }
+
+    public void test5(View view){
+        File file = new File(sdroot, "file1.txt");
+        try {
+            FileOutputStream fout = new FileOutputStream(file);
+            fout.write("OK".getBytes());
+            fout.flush();
+            fout.close();
+        } catch (Exception e) {
+            Log.i("brad", e.toString());
+        }
+
+    }
+
+    public void test6(View view){
+        File file = new File(approot, "file1.txt");
+        try {
+            FileOutputStream fout = new FileOutputStream(file);
+            fout.write("OK".getBytes());
+            fout.flush();
+            fout.close();
+        } catch (Exception e) {
             Log.i("brad", e.toString());
         }
     }
